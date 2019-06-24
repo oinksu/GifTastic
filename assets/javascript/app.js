017ef1c6f77d89279a0ac689655a4eb659265cae
sinid <49916315+oinksu@users.noreply.github.com>
2019-06-24 11:22:36 -0400

    added html placeholder. Materiaze library src link, added API key to JSm file

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+
+
+// Giffy Key = NWabowdySQgQGaZD5HDd9cEZsAbNCkhh
+//
+
+//Giffy jquery code
+//var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=5");
+//xhr.done(function(data) { console.log("success got data", data); });
+
