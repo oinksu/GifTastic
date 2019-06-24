770bd655bc33449d351adbe5a2f7e9299b3658bd
sinid <49916315+oinksu@users.noreply.github.com>
2019-06-24 11:33:27 -0400

    added variables and arrays for functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,3 +7,9 @@
 //var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=5");
 //xhr.done(function(data) { console.log("success got data", data); });
 
+// GIFTASTIC! 
+var topics = ["Sudan", "Yemen", "Rohingya", "Mapuche", "Flint", "western sahara", "Transformers", "Quechua"];
+var button;
+
+var newTopic = ""; 
+
